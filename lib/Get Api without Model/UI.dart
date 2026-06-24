e23633417e43cec2aa7de1426e93bac35a97e1de
sfrabby <fazlerabby6732@gmail.com>
2026-06-24 23:53:36 +0600

    Api without model

---
+++ lib/Get Api without Model/UI.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class getApiWithoutModel extends StatefulWidget {
+  const getApiWithoutModel({super.key});
+
+  @override
+  State<getApiWithoutModel> createState() => _getApiWithoutModelState();
+}
+
+class _getApiWithoutModelState extends State<getApiWithoutModel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+    );
+  }
+}
